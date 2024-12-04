69eff8386bee6bad91e40b4a884de2a2961772e9
Nguyễn Đức Long <longlao159@gmail.com>
2024-12-04 16:51:34 +0700

    fix: Promotion: Remove field

--- ServiceProductPromotion/src/main/java/com/example/demo/entity/PromotionProductDetail.java
+++ ServiceProductPromotion/src/main/java/com/example/demo/entity/PromotionProductDetail.java
@@ -2,42 +2,38 @@ package com.example.demo.entity;
 
 public class PromotionProductDetail {
 	private String barcode;
-	private double shipDiscount;
-	private double pricePercentDiscount;
-	public PromotionProductDetail(String barcode, double shipDiscount, double pricePercentDiscount) {
-		super();
-		this.barcode = barcode;
-		this.shipDiscount = shipDiscount;
-		this.pricePercentDiscount = pricePercentDiscount;
-	}
+	private double priceDiscount;
+
 	public PromotionProductDetail() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	public PromotionProductDetail(String barcode, double priceDiscount) {
+		super();
+		this.barcode = barcode;
+		this.priceDiscount = priceDiscount;
+	}
+
 	public String getBarcode() {
 		return barcode;
 	}
+
 	public void setBarcode(String barcode) {
 		this.barcode = barcode;
 	}
-	public double getShipDiscount() {
-		return shipDiscount;
+
+	public double getPriceDiscount() {
+		return priceDiscount;
 	}
-	public void setShipDiscount(double shipDiscount) {
-		this.shipDiscount = shipDiscount;
+
+	public void setPriceDiscount(double priceDiscount) {
+		this.priceDiscount = priceDiscount;
 	}
-	public double getPricePercentDiscount() {
-		return pricePercentDiscount;
-	}
-	public void setPricePercentDiscount(double pricePercentDiscount) {
-		this.pricePercentDiscount = pricePercentDiscount;
-	}
+
 	@Override
 	public String toString() {
-		return "ProductPromotionDetail [barcode=" + barcode + ", shipDiscount=" + shipDiscount
-				+ ", pricePercentDiscount=" + pricePercentDiscount + "]";
+		return "PromotionProductDetail [barcode=" + barcode + ", priceDiscount=" + priceDiscount + "]";
 	}
-	
-	
-	
+
 }
